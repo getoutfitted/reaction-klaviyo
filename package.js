8adffde2345537a3c6b671131d17981a0b14f5d0
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-04 10:07:45 -0700

    first commit

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+  summary: 'Klaviyo support for Reaction!',
+  name: 'getoutfitted:reaction-klaviyo',
+  version: '0.1.0',
+  git: 'https://github.com/getoutfitted/reaction-klaviyo'
+});
