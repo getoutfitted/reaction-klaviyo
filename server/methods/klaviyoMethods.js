de5a51cccda93a94061c5ec00f377d75bbad46c7
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-07 14:38:22 -0700

    added people method that adds a person to klaviyo

--- server/methods/klaviyoMethods.js
+++ server/methods/klaviyoMethods.js
@@ -15,5 +15,22 @@ Meteor.methods({
           ReactionCore.Log.info('Klaviyo event successfully logged');
         }
       });
+  },
+  'klaviyo/logPerson': function (data) {
+    check(data, String);
+    HTTP.get('https://a.klaviyo.com/api/identify', {
+      params: {
+        'data': data
+      }
+    },
+      function (error, response) {
+        if (error) {
+          let buf = new Buffer(data, 'base64');
+          let decoded = JSON.parse(buf.toString());
+          ReactionCore.Log.warn('Klaviyo person was not logged', decoded);
+        } else {
+          ReactionCore.Log.info('Klaviyo person successfully identified', response);
+        }
+      });
   }
 });
