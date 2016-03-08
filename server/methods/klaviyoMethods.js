cea37e91e04f1a0d6756bb7d38b1401ab4ae791f
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-07 18:04:16 -0700

    people up and running and house changes for events

--- server/methods/klaviyoMethods.js
+++ server/methods/klaviyoMethods.js
@@ -6,15 +6,19 @@ Meteor.methods({
         'data': data
       }
     },
-      function (error, response) {
-        if (error) {
-          let buf = new Buffer(data, 'base64');
-          let decoded = JSON.parse(buf.toString());
-          ReactionCore.Log.warn('Klaviyo event was not logged', decoded);
-        } else {
-          ReactionCore.Log.info('Klaviyo event successfully logged');
-        }
-      });
+    function (error, response) {
+      if (error) {
+        let buf = new Buffer(data, 'base64');
+        let decoded = JSON.parse(buf.toString());
+        ReactionCore.Log.warn('Klaviyo event was not logged', decoded);
+      } else if (response.content === '0') {
+        let buf = new Buffer(data, 'base64');
+        let decoded = JSON.parse(buf.toString());
+        ReactionCore.Log.warn('Klaviyo event was not logged', decoded);
+      } else {
+        ReactionCore.Log.info('Klaviyo event successfully logged');
+      }
+    });
   },
   'klaviyo/logPerson': function (data) {
     check(data, String);
@@ -23,14 +27,18 @@ Meteor.methods({
         'data': data
       }
     },
-      function (error, response) {
-        if (error) {
-          let buf = new Buffer(data, 'base64');
-          let decoded = JSON.parse(buf.toString());
-          ReactionCore.Log.warn('Klaviyo person was not logged', decoded);
-        } else {
-          ReactionCore.Log.info('Klaviyo person successfully identified', response);
-        }
-      });
+    function (error, response) {
+      if (error) {
+        let buf = new Buffer(data, 'base64');
+        let decoded = JSON.parse(buf.toString());
+        ReactionCore.Log.warn('Klaviyo person was not logged', decoded);
+      } else if (response.content === '0') {
+        let buf = new Buffer(data, 'base64');
+        let decoded = JSON.parse(buf.toString());
+        ReactionCore.Log.warn('Klaviyo person was not logged', decoded);
+      } else {
+        ReactionCore.Log.info('Klaviyo person successfully identified', response);
+      }
+    });
   }
 });
