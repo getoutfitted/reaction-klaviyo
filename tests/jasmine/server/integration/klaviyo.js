0464d34cd854a531f8e76676f54f6ffbd8e9315b
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-09 12:43:14 -0700

    trackEvent method tested

--- tests/jasmine/server/integration/klaviyo.js
+++ tests/jasmine/server/integration/klaviyo.js
@@ -48,7 +48,7 @@ describe('Klaviyo', function () {
       const klaviyoEvent = klaviyoEventCreator(user);
       expect(function () {
         Klaviyo.trackEvent(klaviyoEvent);
-      }).toThrowError('403');
+      }).toThrowError('403 Access Denied, Klaviyo is not enabled for this shop.');
     });
 
     it('should throw an error if klaviyo is not enabled', function () {
@@ -59,12 +59,13 @@ describe('Klaviyo', function () {
       const klaviyoEvent = klaviyoEventCreator(user);
       expect(function () {
         Klaviyo.trackEvent(klaviyoEvent);
-      }).toThrowError('403');
+      }).toThrowError('403 Access Denied, Klaviyo is not enabled for this shop.');
     });
 
     it('should throw an error if event has no properties', function () {
       const klaviyoPackage = Factory.create('klaviyoPackage');
       const user = Factory.create('user');
+      spyOn(ReactionCore, 'getShopId').and.returnValue(klaviyoPackage.shopId);
       let event = klaviyoEventCreator(user);
       event = _.omit(event, 'properties');
       expect(function () {
@@ -72,5 +73,27 @@ describe('Klaviyo', function () {
       }).toThrow();
     });
 
+    it('should throw an error if api keys are not configurered', function () {
+      let klaviyoPackage = Factory.create('klaviyoPackage');
+      const user = Factory.create('user');
+      let event = klaviyoEventCreator(user);
+      spyOn(ReactionCore, 'getShopId').and.returnValue(klaviyoPackage.shopId);
+      expect(function () {
+        Klaviyo.trackEvent(event);
+      }).toThrowError('403 Klaviyo API Keys are not configured');
+    });
+
+    it('should call klaviyo/logEvent when it meets all scenarios', function () {
+      let klaviyoPackage = Factory.create('klaviyoPackage', {
+        'settings.api.publicKey': 'FakeKey',
+        'settings.api.privateKey': 'FakeKey'
+      });
+      const user = Factory.create('user');
+      let event = klaviyoEventCreator(user);
+      spyOn(ReactionCore, 'getShopId').and.returnValue(klaviyoPackage.shopId);
+      spyOn(Meteor, 'call');
+      Klaviyo.trackEvent(event);
+      expect(Meteor.call).toHaveBeenCalled();
+    });
   });
 });
