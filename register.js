b92f751639e745afeb49832d92b9be072c78e5a0
Paul Grever <paulgrever@gmail.com>
2016-08-18 15:43:45 -0600

    updated to have layout compatible with our app

--- register.js
+++ register.js
@@ -1,4 +1,6 @@
-ReactionCore.registerPackage({
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
   label: 'Klaviyo',
   name: 'reaction-klaviyo',
   icon: 'fa fa-envelope',
@@ -12,7 +14,7 @@ ReactionCore.registerPackage({
     container: 'getoutfitted',
     icon: 'fa fa-envelope',
     template: 'klaviyoDashboard',
-    workflow: 'coreWorkflow',
+    workflow: 'klaviyoWorkflow',
     priority: 3
   }, {
     route: '/dashboard/klaviyo/settings',
@@ -20,5 +22,32 @@ ReactionCore.registerPackage({
     label: 'Klaviyo Settings',
     name: 'klaviyoSettings',
     template: 'klaviyoSettings'
+  }],
+  layout: [{
+      workflow: "klaviyoWorkflow",
+      layout: "coreLayout",
+      theme: "default",
+      enabled: true,
+      structure: {
+        template: "klaviyoDashboard",
+        layoutHeader: "goLayoutHeader",
+        layoutFooter: "goLayoutFooter",
+        notFound: "goNotFound",
+        dashboardControls: "dashboardControls",
+        adminControlsFooter: "adminControlsFooter"
+      }
+  }, {
+      workflow: "klaviyoWorkflow",
+      layout: "getoutfittedLayout",
+      theme: "default",
+      enabled: true,
+      structure: {
+        template: "klaviyoDashboard",
+        layoutHeader: "goLayoutHeader",
+        layoutFooter: "goLayoutFooter",
+        notFound: "goNotFound",
+        dashboardControls: "dashboardControls",
+        adminControlsFooter: "adminControlsFooter"
+      }
   }]
 });
